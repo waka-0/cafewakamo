711f717dde8ed250635d88ed2fb4ce739666729a
momoka <149499920+waka-0@users.noreply.github.com>
2024-10-31 15:03:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
 
-const menuBtn = document.querySelector(".menu-btn");
-const overlayBox = document.querySelector(".overlay");
-const menuLine = document.querySelector(".menu-line");
-const navElement = document.querySelectorAll(".nav");
-
-menuBtn.addEventListener("click",() => {
-  overlayBox.classList.toggle("show");
-  menuLine.classList.toggle("active");
-});
-
-navElement.forEach((nav) => {
-  nav.addEventListener("click", () => {
-    overlayBox.classList.remove("show");
-    menuLine.classList.remove("active");
-  })
-})
